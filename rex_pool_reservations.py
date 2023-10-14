db97bccf596638e3f7ed83df57fb9faf53db3446
Brian Bentley <bbentley@mailworks.org>
2023-10-14 11:10:28 -0400

    Updates for new version of selenium

--- rex_pool_reservations.py
+++ rex_pool_reservations.py
@@ -7,6 +7,7 @@ import smtplib
 import sys
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
@@ -55,20 +56,20 @@ def parse_config(config_file):
 def login(web_driver, username, password):
     """Log in to the portal."""
     logging.info("logging on to portal.")
-    username_field = web_driver.find_element_by_id(
+    username_field = web_driver.find_element(By.ID, 
         "ctl00_pageContentHolder_loginControl_UserName"
     )
-    password_field = web_driver.find_element_by_id(
+    password_field = web_driver.find_element(By.ID, 
         "ctl00_pageContentHolder_loginControl_Password"
     )
-    login_button = web_driver.find_element_by_id(
+    login_button = web_driver.find_element(By.ID, 
         "ctl00_pageContentHolder_loginControl_Login"
     )
     username_field.send_keys(username)
     password_field.send_keys(password)
     login_button.click()
     WebDriverWait(web_driver, timeout=30).until(
-        lambda d: d.find_element_by_id("ctl00_welcomeCnt_lbWelcome")
+        lambda d: d.find_element(By.ID, "ctl00_welcomeCnt_lbWelcome")
     )
     logging.info("login succesful.")
 
@@ -79,41 +80,41 @@ def pick_date(calender_element, target_date):
     today = datetime.date.today()
 
     if today.day > target_date.day:
-        calender_element.find_element_by_xpath("//a[@title='Next']").click()
+        calender_element.find_element(By.XPATH, "//a[@title='Next']").click()
 
-    calender_element.find_element_by_link_text(str(target_date.day)).click()
+    calender_element.find_element(By.LINK_TEXT, str(target_date.day)).click()
 
 
 def navigate_to_reservation_page(web_driver):
     """navigates to the reservation page"""
     logging.info("Navigating to the reservation page.")
-    web_driver.find_element_by_id("menu_SCH").click()
+    web_driver.find_element(By.ID, "menu_SCH").click()
 
     WebDriverWait(web_driver, timeout=30).until(
-        lambda d: d.find_element_by_xpath("//div[@title='Aquatics']")
+        lambda d: d.find_element(By.XPATH, "//div[@title='Aquatics']")
     )
-    web_driver.find_element_by_xpath("//div[@title='Aquatics']").click()
+    web_driver.find_element(By.XPATH, "//div[@title='Aquatics']").click()
 
     WebDriverWait(web_driver, timeout=30).until(
-        lambda d: d.find_element_by_xpath("//div[@title='A  Lap Pool Reservation']")
+        lambda d: d.find_element(By.XPATH, "//div[@title='A  Lap Pool Reservation']")
     )
-    web_driver.find_element_by_xpath("//div[@title='A  Lap Pool Reservation']").click()
+    web_driver.find_element(By.XPATH, "//div[@title='A  Lap Pool Reservation']").click()
 
     WebDriverWait(web_driver, timeout=30).until(
-        lambda d: d.find_element_by_id("dateControlText")
+        lambda d: d.find_element(By.ID, "dateControlText")
     )
 
-    web_driver.find_elements_by_class_name("ui-datepicker-trigger")[0].click()
+    web_driver.find_elements(By.CLASS_NAME, "ui-datepicker-trigger")[0].click()
 
     WebDriverWait(web_driver, timeout=30).until(
-        lambda d: d.find_element_by_id("ui-datepicker-div")
+        lambda d: d.find_element(By.ID, "ui-datepicker-div")
     )
-    calender_element = web_driver.find_element_by_id("ui-datepicker-div")
+    calender_element = web_driver.find_element(By.ID, "ui-datepicker-div")
 
     target_date = datetime.date.today() + datetime.timedelta(days=7)
     pick_date(calender_element, target_date)
 
-    web_driver.find_element_by_id("btnContinue").click()
+    web_driver.find_element(By.ID, "btnContinue").click()
 
 
 def schedule_pool_time(web_driver, config):
@@ -121,11 +122,11 @@ def schedule_pool_time(web_driver, config):
     navigate_to_reservation_page(web_driver)
 
     WebDriverWait(web_driver, timeout=30).until(
-        lambda d: d.find_element_by_id("spnWeekDate")
+        lambda d: d.find_element(By.ID, "spnWeekDate")
     )
     # TODO: This should probably be changed to an element wait
     time.sleep(5)
-    web_driver.find_element_by_id("ancSchListView").click()
+    web_driver.find_element(By.ID, "ancSchListView").click()
 
     current_weekday = datetime.datetime.today().weekday()
 
@@ -150,39 +151,39 @@ def schedule_pool_time(web_driver, config):
 
             while found_pool_time == False:
                 WebDriverWait(web_driver, timeout=30).until(
-                    lambda d: d.find_element_by_class_name("tblSchslots")
+                    lambda d: d.find_element(By.CLASS_NAME, "tblSchslots")
                 )
 
-                schedule_table = web_driver.find_element_by_class_name("tblSchslots")
+                schedule_table = web_driver.find_element(By.CLASS_NAME, "tblSchslots")
                 try:
-                    schedule_table.find_element_by_xpath(
+                    schedule_table.find_element(By.XPATH, 
                         schedule_xpath
-                    ).find_element_by_xpath("..").find_element_by_class_name(
+                    ).find_element(By.XPATH, "..").find_element(By.CLASS_NAME, 
                         "schTblButton"
                     ).click()
                     found_pool_time = True
                 except NoSuchElementException:
                     try:
-                        web_driver.find_element_by_id("ancSchListNext").click()
+                        web_driver.find_element(By.ID, "ancSchListNext").click()
                     except ElementNotInteractableException:
                         raise PoolReservationNoMatchTimeError(
                             "Unable to find matching pool time."
                         )
 
             WebDriverWait(web_driver, timeout=30).until(
-                lambda d: d.find_element_by_id("btnContinue")
+                lambda d: d.find_element(By.ID, "btnContinue")
             )
-            web_driver.find_element_by_id("btnContinue").click()
+            web_driver.find_element(By.ID, "btnContinue").click()
 
             family_member_xpath = f"//td[contains(text(), '{config['family_member']}')]"
 
             WebDriverWait(web_driver, timeout=30).until(
-                lambda d: d.find_element_by_xpath(family_member_xpath)
+                lambda d: d.find_element(By.XPATH, family_member_xpath)
             )
 
-            web_driver.find_element_by_xpath(family_member_xpath).find_element_by_xpath(
+            web_driver.find_element(By.XPATH, family_member_xpath).find_element(By.XPATH, 
                 ".."
-            ).find_element_by_xpath("//label[contains(@for, '_rdoHost')]").click()
+            ).find_element(By.XPATH, "//label[contains(@for, '_rdoHost')]").click()
 
             break
 
@@ -193,16 +194,16 @@ def schedule_pool_time(web_driver, config):
 
     try:
         WebDriverWait(web_driver, timeout=30).until(
-            lambda d: d.find_element_by_id("ctl00_pageContentHolder_btnContinueCart")
+            lambda d: d.find_element(By.ID, "ctl00_pageContentHolder_btnContinueCart")
         )
-        web_driver.find_element_by_id("ctl00_pageContentHolder_btnContinueCart").click()
+        web_driver.find_element(By.ID, "ctl00_pageContentHolder_btnContinueCart").click()
     except:
         raise PoolReservationError("Unable to cotinue with scheduled time in cart.")
     try:
         WebDriverWait(web_driver, timeout=30).until(
-            lambda d: d.find_element_by_id("ctl00_pageContentHolder_ScheduleDetails")
+            lambda d: d.find_element(By.ID, "ctl00_pageContentHolder_ScheduleDetails")
         )
-        reservation_message = web_driver.find_element_by_id(
+        reservation_message = web_driver.find_element(By.ID, 
             "ctl00_pageContentHolder_ScheduleDetails"
         ).text
         logging.info(reservation_message)
